5cd3f9e7a0390e12d04ca42ffbac310b8a65142f
ValterMonteiro <valtinms@gmail.com>
2023-07-13 13:16:30 -0400

    inicio do projeto

---
+++ gestaoE/src/main/java/com/gestaoE/gestaoE/GestaoEApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoE.gestaoE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEApplication.class, args);
+	}
+
+}
---
+++ gestaoE/src/test/java/com/gestaoE/gestaoE/GestaoEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoE.gestaoE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
