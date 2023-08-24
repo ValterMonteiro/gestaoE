aad04dbb2950c3146a2d01aa6e226810178d397b
ValterMonteiro <valtinms@gmail.com>
2023-08-24 19:39:44 -0400

    criando e implementando crud Cliente

---
+++ src/main/java/br/com/gestaoe/cliente/Cliente.java
@@ -0,0 +1,173 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.gestaoe.cliente;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * 
+ */
+@Entity
+@Table(name = "tb_produto")
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private String rua;
+	private Integer numero;
+	private String bairro;
+	private String cidade;
+	private Long cep;
+	private String uf;
+	private Long telefone;
+	private String email;
+	
+	
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cliente(Long id, String razaoSocial, String cnpj, String rua, Integer numero, String bairro,
+			String cidade, Long cep, String uf, Long telefone, String email) {
+		this.id = id;
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.cep = cep;
+		this.uf = uf;
+		this.telefone = telefone;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public Long getCep() {
+		return cep;
+	}
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public Long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bairro, cep, cidade, cnpj, email, id, numero, razaoSocial, rua, telefone, uf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(cidade, other.cidade) && Objects.equals(cnpj, other.cnpj)
+				&& Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(numero, other.numero) && Objects.equals(razaoSocial, other.razaoSocial)
+				&& Objects.equals(rua, other.rua) && Objects.equals(telefone, other.telefone)
+				&& Objects.equals(uf, other.uf);
+	}
+
+
+}
---
+++ src/main/java/br/com/gestaoe/cliente/ClienteService.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.gestaoe.cliente;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+    
+    @Transactional(readOnly = true)
+	public List<ClienteDto> findAll(){
+		List<Cliente> lista = repository.findAll();
+		return lista.stream().map(x -> new ClienteDto(x)).collect(Collectors.toList());
+	}
+    
+    
+    @Transactional(readOnly = true)
+	public ClienteDto findById(Long id) {
+		Optional<Cliente> obj = repository.findById(id);
+		
+		Cliente entity = obj.orElseThrow(() -> new ResourceNotFoundException("O registro solicitado não foi localizado."));
+		return new ClienteDto(entity);		
+	}
+
+	@Transactional
+	public ClienteDto insert(ClienteDto dto) {
+		Cliente entity = new Cliente();
+		
+		entity.setRazaoSocial(dto.getRazaoSocial());
+		entity.setCnpj(dto.getCnpj());
+		entity.setRua(dto.getRua());
+		entity.setNumero(dto.getNumero()); 
+		entity.setBairro(dto.getBairro());
+		entity.setCidade(dto.getCidade()); 
+		entity.setCep(dto.getCep()); 
+		entity.setUf(dto.getUf());
+		entity.setTelefone(dto.getTelefone()); 
+		entity.setEmail(dto.getEmail());
+		
+		entity = repository.save(entity);
+
+		return new ClienteDto(entity);
+	}
+
+	@Transactional
+	public ClienteDto update(Long id, ClienteDto dto) {
+		
+		try {
+			Cliente entity = repository.getReferenceById(id);
+			
+			entity.setRazaoSocial(dto.getRazaoSocial());
+			entity.setCnpj(dto.getCnpj());
+			entity.setRua(dto.getRua());
+			entity.setNumero(dto.getNumero()); 
+			entity.setBairro(dto.getBairro());
+			entity.setCidade(dto.getCidade()); 
+			entity.setCep(dto.getCep()); 
+			entity.setUf(dto.getUf());
+			entity.setTelefone(dto.getTelefone()); 
+			entity.setEmail(dto.getEmail());
+			
+			entity = repository.save(entity);
+	
+			return new ClienteDto(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(
+					"O recurso com o ID "+id+" não foi localizado");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		if(repository.existsById(id)) {
+			repository.deleteById(id);
+		}
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("O registro solicitado não foi localizado.");
+		}
+	}
+     
+}
