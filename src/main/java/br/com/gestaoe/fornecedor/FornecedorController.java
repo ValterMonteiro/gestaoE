189baa72d7d35363406bdf70b42c99f0679204ba
ValterMonteiro <valtinms@gmail.com>
2023-08-24 19:33:57 -0400

    criando e implementando crud Fornecedor

---
+++ src/main/java/br/com/gestaoe/fornecedor/FornecedorController.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.gestaoe.fornecedor;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class FornecedorController {
+
+    @Autowired
+    private FornecedorService service;
+    
+    @GetMapping
+    public ResponseEntity<List<FornecedorDto>> findAll(){
+    	List<FornecedorDto> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+    }
+    
+    
+    @GetMapping(value = "/{id}")
+	public ResponseEntity<FornecedorDto> findById(@PathVariable Long id){
+		FornecedorDto dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<FornecedorDto> insert(@RequestBody FornecedorDto Dto){
+		Dto = service.insert(Dto);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(Dto.getId())
+					.toUri();
+		return ResponseEntity.created(uri).body(null);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<FornecedorDto> update(
+			@PathVariable Long id,
+			@RequestBody FornecedorDto Dto){
+		Dto = service.update(id, Dto);
+		return ResponseEntity.ok().body(Dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+    
+    
+    
+}
